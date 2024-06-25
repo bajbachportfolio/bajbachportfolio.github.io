117b55dca37b422a5cee5ddf710ba4ff3025d13b
Alexandr Baibachenko <alex.bajbachenko@cheitgroup.com>
2024-06-25 18:37:35 +0300

    fix sw path

--- iqclinic_PS_opt/assets/js/custom.js
+++ iqclinic_PS_opt/assets/js/custom.js
@@ -207,7 +207,7 @@ $(document).ready(function() {
 window.addEventListener('load', async () => {
 	if ('serviceWorker' in navigator) {
 	  try {
-		const reg = await navigator.serviceWorker.register('/../serviceWorker.js')
+		const reg = await navigator.serviceWorker.register('/../../serviceWorker.js')
 		console.log('Service worker register success', reg)
 	  } catch (e) {
 		console.log('Service worker register fail')
