f5d622a27c11f694a625c7525af6be764af8b41a
Alexandr Baibachenko <alex.bajbachenko@cheitgroup.com>
2024-06-25 18:27:42 +0300

    feat: service worker - sw - registered

--- iqclinic_PS_opt/assets/js/custom.js
+++ iqclinic_PS_opt/assets/js/custom.js
@@ -203,3 +203,14 @@ $(document).ready(function() {
 				return false;
 			});
 });
+
+window.addEventListener('load', async () => {
+	if ('serviceWorker' in navigator) {
+	  try {
+		const reg = await navigator.serviceWorker.register('../../serviceWorker.js')
+		console.log('Service worker register success', reg)
+	  } catch (e) {
+		console.log('Service worker register fail')
+	  }
+	}
+  })
