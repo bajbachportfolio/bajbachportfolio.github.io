dad1fa4e55c643b3770ba2134ce730f19dfe96d9
Alexandr Baibachenko <alex.bajbachenko@cheitgroup.com>
2024-06-26 16:31:58 +0300

    sw update

--- iqclinic_PS_opt/sw.js
+++ iqclinic_PS_opt/sw.js
@@ -23,18 +23,30 @@ self.addEventListener('activate', async event => {
   });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(cacheFirst(event.request))
-    // const {request} = event
+    // event.respondWith(cacheFirst(event.request))
+    const {request} = event
   
-    // const url = new URL(request.url)
-    // if (url.origin === location.origin) {
-    //   event.respondWith(cacheFirst(request))
-    // } else {
-    //   event.respondWith(networkFirst(request))
-    // }
+    const url = new URL(request.url)
+    if (url.origin === location.origin) {
+      event.respondWith(cacheFirst(request))
+    } else {
+      event.respondWith(networkFirst(request))
+    }
   })
 
   async function cacheFirst(request) {
     const cached = await caches.match(request)
     return cached ?? await fetch(request)
+  }
+
+  async function networkFirst(request) {
+    const cache = await caches.open(dynamicCacheName)
+    try {
+      const response = await fetch(request)
+      await cache.put(request, response.clone())
+      return response
+    } catch (e) {
+      const cached = await cache.match(request)
+      return cached ?? await caches.match('/offline.html')
+    }
   }
